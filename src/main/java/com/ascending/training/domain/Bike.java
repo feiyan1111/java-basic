448a33ed8db055b3e638fb849b30baa976bc9847
feiyan1111 <fei48993715@gwu.edu>
2019-05-23 14:52:30 -0400

    Bike

---
+++ src/main/java/com/ascending/training/domain/Bike.java
@@ -0,0 +1,33 @@
+package com.ascending.training.domain;
+
+public class Bike {
+    public String wheelShape = "round";
+    public int wheelNum = 2;
+    public String seatShape = "triangle";
+
+    public Bike() {
+    }
+
+    public Bike(String wheelShape, int wheelNum, String seatShape) {
+        this.wheelNum = wheelNum;
+        this.wheelShape = wheelShape;
+        this.seatShape = seatShape;
+    }
+
+
+
+
+        public static void main (String[]args){
+        Bike bike = new Bike();
+        System.out.println(bike.seatShape);
+        System.out.println(bike.wheelNum);
+        System.out.println(bike.wheelShape);
+        Bike bike2 = new Bike("square", 3, "triangle");
+        System.out.println(bike2.wheelShape);
+        System.out.println(bike2.wheelNum);
+        System.out.println(bike2.seatShape);
+    }
+
+}
+
+

4e3d1c6512f099c01a8f27586debf03d3be18dab
feiyan1111 <fei48993715@gwu.edu>
2019-05-24 13:03:47 -0400

    Motorcycle

---
+++ src/main/java/com/ascending/training/domain/Motorcycle.java
@@ -0,0 +1,35 @@
+package com.ascending.training.domain;
+
+public class Motorcycle extends Bicycle {
+
+    public int gastank = 10;
+    public String motor = "big";
+
+    public Motorcycle() {}
+    public Motorcycle(String wheelShape, int wheelNum, String seatShape, int pedalNum, int gastank, String motor) {
+//        this.wheelNum = wheelNum;
+//        this.wheelShape = wheelShape;
+//        this.seatShape = seatShape;
+        super(wheelShape,wheelNum,seatShape,pedalNum);
+        this.gastank = gastank;
+        this.motor = motor;
+    }
+
+    public static void main (String[]args){
+        Motorcycle motorcycle = new Motorcycle();
+        System.out.println(motorcycle.seatShape);
+        System.out.println(motorcycle.wheelNum);
+        System.out.println(motorcycle.wheelShape);
+        System.out.println(motorcycle.pedalNum);
+        System.out.println(motorcycle.gastank);
+        System.out.println(motorcycle.motor);
+        Motorcycle motorcycle2 = new Motorcycle("round", 4, "square",2, 10, "big");
+        System.out.println(motorcycle2.wheelShape);
+        System.out.println(motorcycle2.wheelNum);
+        System.out.println(motorcycle2.seatShape);
+        System.out.println(motorcycle2.pedalNum);
+        System.out.println(motorcycle2.pedalNum);
+        System.out.println(motorcycle2.gastank);
+
+    }
+}
